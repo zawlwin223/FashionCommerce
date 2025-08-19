a0085ab49c2f884a7eb22a496c70e3f5872c50fb
zawlwin223 <zawlwinp223@gmail.com>
2025-08-19 07:56:15 -0700

    feat:added slider in hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,27 +1,105 @@
 import '../style/hero.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
-import image from '../img/kisspng-tunic-clothing-free-people-dress-coat-png-bayan-resimleri-tubes-femes-png-woman-fotos-5d082eaa562755.1772136015608173223529.png'
+import image1 from '../img/kisspng-tunic-clothing-free-people-dress-coat-png-bayan-resimleri-tubes-femes-png-woman-fotos-5d082eaa562755.1772136015608173223529.png'
+import image2 from '../img/pngwingFahionBoy.png'
+import image3 from '../img/pngwingMan1.png'
+import image4 from '../img/pngwingWomanFashion.png'
 import LinkButton from '../component/LinkButton'
-export default function Hero() {
-  return (
-    <>
-      <section className="hero">
-        <div>
-          <p>Trad-in-off</p>
-          <h1>
-            Super value deals <br />
-            On all products
-          </h1>
-          <p>Save more with coupon &70 % off</p>
 
-          <LinkButton path="/shop" halfWidth={true}>
-            Go To Shop
-          </LinkButton>
+import Slider from 'react-slick'
+export default function Hero() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  }
+  return (
+    <section className="hero">
+      <Slider {...settings}>
+        <div>
+          <div className="hero_slider">
+            <div>
+              <p>Trad-in-off</p>
+              <h1>
+                Super value deals <br />
+                On all products
+              </h1>
+              <p>Save more with coupon &70 % off</p>
+
+              <LinkButton path="/shop" halfWidth={true}>
+                Go To Shop
+              </LinkButton>
+            </div>
+            <div>
+              <img src={image1} alt="" className="hero_img" />
+            </div>
+          </div>
         </div>
         <div>
-          <img src={image} alt="" />
+          <div className="hero_slider">
+            <div>
+              <p>Trad-in-off</p>
+              <h1>
+                Super value deals <br />
+                On all products
+              </h1>
+              <p>Save more with coupon &70 % off</p>
+
+              <LinkButton path="/shop" halfWidth={true}>
+                Go To Shop
+              </LinkButton>
+            </div>
+            <div>
+              <img src={image2} className="hero_img" alt="" />
+            </div>
+          </div>
         </div>
-      </section>
-    </>
+        <div>
+          <div className="hero_slider">
+            <div>
+              <p>Trad-in-off</p>
+              <h1>
+                Super value deals <br />
+                On all products
+              </h1>
+              <p>Save more with coupon &70 % off</p>
+
+              <LinkButton path="/shop" halfWidth={true}>
+                Go To Shop
+              </LinkButton>
+            </div>
+            <div>
+              <img src={image3} className="hero_img" alt="" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="hero_slider">
+            <div>
+              <p>Trad-in-off</p>
+              <h1>
+                Super value deals <br />
+                On all products
+              </h1>
+              <p>Save more with coupon &70 % off</p>
+
+              <LinkButton path="/shop" halfWidth={true}>
+                Go To Shop
+              </LinkButton>
+            </div>
+            <div>
+              <img src={image4} className="hero_img" alt="" />
+            </div>
+          </div>
+        </div>
+      </Slider>
+    </section>
   )
 }
